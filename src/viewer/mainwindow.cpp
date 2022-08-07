859c322e076afc25f024b1b86975be1ce9e457c6
GrusnyDance <grusny.dance@gmail.com>
2022-08-07 21:16:08 +0200

    it uploads the cow

--- src/viewer/mainwindow.cpp
+++ src/viewer/mainwindow.cpp
@@ -14,11 +14,35 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    Qt3DCore::QEntity *rootEntity = new Qt3DCore::QEntity();
+
 
     Qt3DExtras::Qt3DWindow *view = new Qt3DExtras::Qt3DWindow();
     view->defaultFrameGraph()->setClearColor(QRgb(0x1B2732));
+    view->setRootEntity(rootEntity);
     QWidget *container = QWidget::createWindowContainer(view);
     ui->gridLayout_4->addWidget(container,1,0);
+
+    Qt3DRender::QCamera *cameraEntity = view->camera();
+    // X+ -> right
+    // Y+ -> away
+    // Z+ -> up
+    cameraEntity->lens()->setPerspectiveProjection(
+        45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
+    cameraEntity->setPosition(QVector3D(0, -5, 5.0f));
+    cameraEntity->setUpVector(QVector3D(0, 0, 1));
+    cameraEntity->setViewCenter(QVector3D(0, 0, 0));
+
+
+    Qt3DExtras::QOrbitCameraController *camController = new
+        Qt3DExtras::QOrbitCameraController(rootEntity);
+    camController->setCamera(cameraEntity);
+
+    Qt3DCore::QEntity *sceneLoaderEntity = new Qt3DCore::QEntity(rootEntity);
+    Qt3DRender::QSceneLoader *sceneLoader = new Qt3DRender::QSceneLoader(sceneLoaderEntity);
+    sceneLoader->setObjectName("cow.obj");
+    sceneLoaderEntity->addComponent(sceneLoader);
+    sceneLoader->setSource(QUrl(QString("file:/home/darika/view/src/viewer/cow.obj")));
 
 
     ui->FileName->setStyleSheet("background-color: transparent; color: lavender");
--- src/viewer/viewer.pro
+++ src/viewer/viewer.pro
@@ -2,8 +2,6 @@ QT       += core gui 3dcore 3drender 3dinput 3dextras
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
-CONFIG += c++17
-
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
