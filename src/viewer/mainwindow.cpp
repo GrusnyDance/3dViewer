e300f2ac49f5252b8b6cb5bdbe283a75955882d8
GrusnyDance <grusny.dance@gmail.com>
2022-08-07 17:32:40 +0200

    button choose file works

--- src/viewer/mainwindow.cpp
+++ src/viewer/mainwindow.cpp
@@ -2,27 +2,26 @@
 #include "ui_mainwindow.h"
 #include <QScreen>
 #include <QRect>
-#include <QLayout>
+#include <QFileDialog>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->FileName->setStyleSheet("background-color: transparent;");
+    ui->FileName->setStyleSheet("background-color: transparent; color: lavender");
     ui->LineVert->setStyleSheet("background-color: transparent;");
     ui->LineEdges->setStyleSheet("background-color: transparent;");
 
     QScreen *screen =
-        QGuiApplication::primaryScreen();  // здесь и следующие 4 строки - задание
-                                           // размера в пропорциях относительно
-                                           // экрана устройства, на котором
-                                           // открыта программа
+        QGuiApplication::primaryScreen();
     QRect screenGeometry = screen->geometry();
     int height = screenGeometry.height();
     int width = screenGeometry.width();
     resize(width * 0.8, height * 0.9);
 
+    connect(ui->FileButton, SIGNAL(pressed()), this, SLOT(FilePressed()));
 
 }
 
@@ -31,3 +30,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::FilePressed() {
+    QString FindFile = QFileDialog::getOpenFileName(this,
+                       tr("Select a file to open"), QDir::homePath(), tr("Text Files (*.obj)"));
+    ui->FileName->setText(FindFile);
+}
